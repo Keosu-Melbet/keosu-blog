1b3d7c8e4e1dc6df84b6d8c0fdfa4651d7553e42
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-13 22:44:41 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,33 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, BooleanField, SubmitField
+from wtforms import StringField, TextAreaField, SelectField, BooleanField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
-from models import Category
 from flask_wtf.file import FileField, FileAllowed
-
-class ArticleForm(FlaskForm):
-    # ... các trường khác
-    image_file = FileField('Ảnh đại diện', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Chỉ chấp nhận ảnh!')])
-
+from models import Category
 
 class ArticleForm(FlaskForm):
     title = StringField('Tiêu đề', validators=[DataRequired(), Length(max=200)])
     content = TextAreaField('Nội dung', validators=[DataRequired()])
     excerpt = TextAreaField('Tóm tắt', validators=[Length(max=300)])
     category_id = SelectField('Chuyên mục', coerce=int, validators=[DataRequired()])
-    featured_image = StringField('Ảnh đại diện (URL)')
+    
+    # ✅ Upload ảnh đại diện
+    featured_image = FileField('Ảnh đại diện', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Chỉ chấp nhận ảnh!')
+    ])
+    
     featured = BooleanField('Bài viết nổi bật')
     published = BooleanField('Xuất bản', default=True)
-    
+
     # SEO fields
     meta_title = StringField('Meta Title', validators=[Length(max=200)])
     meta_description = TextAreaField('Meta Description', validators=[Length(max=300)])
     meta_keywords = StringField('Meta Keywords', validators=[Length(max=500)])
-    
+
     submit = SubmitField('Lưu bài viết')
-    
+
     def __init__(self, *args, **kwargs):
         super(ArticleForm, self).__init__(*args, **kwargs)
-        # Populate category choices
         self.category_id.choices = [(cat.id, cat.name) for cat in Category.query.all()]
 
 class ContactForm(FlaskForm):
@@ -40,9 +39,8 @@ class ContactForm(FlaskForm):
 class SearchForm(FlaskForm):
     query = StringField('Tìm kiếm...', validators=[DataRequired(), Length(max=100)])
     submit = SubmitField('Tìm kiếm')
-    
+
 class LoginForm(FlaskForm):
     username = StringField('Tên đăng nhập', validators=[DataRequired(), Length(max=100)])
-    password = StringField('Mật khẩu', validators=[DataRequired(), Length(max=100)])
+    password = PasswordField('Mật khẩu', validators=[DataRequired(), Length(max=100)])
     submit = SubmitField('Đăng nhập')
-
