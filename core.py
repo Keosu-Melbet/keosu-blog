ef57722afae71f00e5e209570db1fe45f5b81ced
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-15 01:05:46 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,15 +1,2 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-app = Flask(__name__)  # <-- Khai báo ở ngoài hàm
-
-# Cấu hình database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-db.init_app(app)
-
-from routes import main_bp
-app.register_blueprint(main_bp)
