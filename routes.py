f51cfa236cea8711823e9f743c76d151e68d42f4
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 21:46:22 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+from core import app
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from models import db, Admin, Article, Category, BettingOdd, Match
