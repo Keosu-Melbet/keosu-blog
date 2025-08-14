e68bb0f141348a3da39cbc2612fa7cd1edfaf196
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 21:44:33 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,10 +1,12 @@
 # core.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
