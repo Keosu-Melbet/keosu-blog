6103c2a273566921582a52df4636dda8f3908daf
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 16:04:41 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
+import os
 from flask import Flask
 from .extensions import db
-from .routes import main_bp  # Giả sử bạn dùng Blueprint tên main_bp
+from .routes import main_bp
 
 def create_app():
-    app = Flask(__name__)
+    # Xác định đường dẫn tuyệt đối đến thư mục templates
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    
+    app = Flask(__name__, template_folder=template_dir)
     app.config.from_object('config.Config')
 
     db.init_app(app)
-
-    # Đăng ký Blueprint
     app.register_blueprint(main_bp)
 
     with app.app_context():
         db.create_all()
 
     return app
-
