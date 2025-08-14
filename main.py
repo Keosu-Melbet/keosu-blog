2b539506656054c6231e8d6ce687740176f35c93
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 14:22:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+from app import create_app  # hoặc từ đúng module bạn đặt
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
