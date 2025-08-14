bfcf73ac5cd6632aa1276bfda0e76e09cae8f461
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-15 02:35:54 +0300

    Update core.py

--- core.py
+++ core.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from werkzeug.middleware.proxy_fix import ProxyFix
+from routes import main_bp
+app.register_blueprint(main_bp)
 
 db = SQLAlchemy()
 login_manager = LoginManager()
