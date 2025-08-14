7203116770edf9a9ba2b7ebe078cf3f683a080c2
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 03:05:17 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -11,7 +11,7 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def home():
-    return render_template('home.html')  # hoặc return 'Hello'
+    return 'Hello from Blueprint!'
 
 @bp.route('/<slug>')
 def category_articles_en(slug):
