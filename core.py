e8d1bb9244bf9db50965bfc9e89a3cab3f4bd57d
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 18:32:23 +0300

    Create core.py

---
+++ core.py
@@ -0,0 +1,13 @@
+# core.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'admin_login'
