4d540bc34f1acb9b8727282c87f971e65ee400f2
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 14:27:07 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///keosu.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_recycle": 300,
+        "pool_pre_ping": True,
+    }
