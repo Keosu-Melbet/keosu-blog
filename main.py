d5d2fd2879558a49d2579afbe69efdfe203b8c88
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-14 15:37:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
 from app import create_app
-from app import routes  # ✅ Import để Flask biết đến route
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
+
