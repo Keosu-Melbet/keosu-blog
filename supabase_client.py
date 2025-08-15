b19fe7e7e87b74301e173b06aba46a31e40ba3e9
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-15 14:03:40 +0300

    Create supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,8 @@
+# supabase_client.py
+import os
+from supabase import create_client
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
