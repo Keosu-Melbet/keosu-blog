26a667863d8a4ff15f8000f443ef60d291da7cb0
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-08-16 21:15:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SECRET_KEY = os.getenv("0eda34defbecee982d6aac7c272c74d6ccfad5f6051fc6d30ae18b08782dcc25", "default-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:Tieungoandong@@3979@db.ywvgigmodfrnovbgraqm.supabase.co:5432/postgres")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
